a3eb10e7005279c0862eb697b78af5e6e215ad2d
Pierre Chateau <pierre@chateau.se>
2025-01-02 16:28:23 +0100

    added hamburger menu and fonts

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./Navbar.css";
 export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav>
       <Link to="/" className="title">
         Navbar-Lab
       </Link>
-      <ul>
+      <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={menuOpen ? "open" : ""}>
         <li>
           <NavLink to="/services">Services</NavLink>
         </li>
