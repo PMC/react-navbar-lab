e198b275cc656e3b1740099d0fe900bc3583c2de
Pierre Chateau <pierre@chateau.se>
2025-01-02 14:30:52 +0100

    use NavLink instead

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Navbar.css";
 export const Navbar = () => {
   return (
@@ -9,13 +9,13 @@ export const Navbar = () => {
       </Link>
       <ul>
         <li>
-          <Link to="/services">Services</Link>
+          <NavLink to="/services">Services</NavLink>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <NavLink to="/about">About</NavLink>
         </li>
         <li>
-          <Link to="/contact">Contact</Link>
+          <NavLink to="/contact">Contact</NavLink>
         </li>
       </ul>
     </nav>
